ab3c1e8c86b1786645b8ff7c8eef4fc44f530bdd
Jannik-dev <dev.janniknorden@gmail.com>
2022-08-18 18:21:45 +0200

    Should work

--- .github/bin/build.sh
+++ .github/bin/build.sh
@@ -16,7 +16,8 @@ for CONF in $CONFIGURATIONS; do
     CONF_FOLDER=$(basename ${CONF})
     CONF_COVER=$(sed -n 's/COVER=//p' properties.ini)
     CONF_ABBREVIATIONS_FILE=$(sed -n 's/ABBREVIATIONS_FILE=//p' properties.ini)
-    CONF_MD_FILES=${(sed -n 's/MD_FILES=//p' properties.ini):-*.md}
+    CONF_MD_FILES=$(sed -n 's/MD_FILES=//p' properties.ini)
+    CONF_MD_FILES=${CONF_MD_FILES:-*.md}
     CONF_STYLES=$(sed -n 's/STYLES=//p' properties.ini)
 
     for STYLE in ${CONF_STYLES}; do
@@ -36,7 +37,7 @@ for CONF in $CONFIGURATIONS; do
 
         echo "Combine PDF for ${CONF_FOLDER} with ${STYLE}"
         pdftk ${CONF_COVER} ${STYLE}.pdf cat output ../build/${CONF_FOLDER}/${CONF_FOLDER}_${STYLE}.pdf
-        
+
         cd ..
     done
 done
