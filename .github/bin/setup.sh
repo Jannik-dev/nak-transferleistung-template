d93249dd9150c03513e10b82a853f2e580d036ba
Jannik-dev <dev.janniknorden@gmail.com>
2022-08-18 18:12:19 +0200

    Add empty lines for better readability

--- .github/bin/setup.sh
+++ .github/bin/setup.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/env bash
+
 cd $1
-mkdir build
+
 CONFIGURATIONS=$(find ~+ -type f -name 'properties.ini' -exec dirname -z "{}" \; | sed -z 's/$/\n/')
+
+mkdir build
 cd build
+
 for CONF in $CONFIGURATIONS; do
     CONF_FOLDER=$(basename ${CONF})
     echo "Creating dir build/${CONF_FOLDER}"
 
     mkdir ${CONF_FOLDER}
 done
+
 cd ..
