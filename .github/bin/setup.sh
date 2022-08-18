9603b202bc1debd65e3c34c79c8ddfdbd9c89aab
Jannik-dev <dev.janniknorden@gmail.com>
2022-08-18 18:04:52 +0200

    Remove spaces

--- .github/bin/setup.sh
+++ .github/bin/setup.sh
@@ -1,16 +1,12 @@
 #!/usr/bin/env bash
-
-cd ${1}
-
+cd $1
 mkdir build
 CONFIGURATIONS=$(find ~+ -type f -name 'config.ini' -exec dirname -z "{}" \; | sed -z 's/$/\n/')
 cd build
-
 for CONF in $CONFIGURATIONS; do
     CONF_FOLDER=$(basename ${CONF})
     echo "Creating dir build/${CONF_FOLDER}"
 
     mkdir ${CONF_FOLDER}
 done
-
 cd ..
