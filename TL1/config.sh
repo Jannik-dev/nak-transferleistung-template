eb25c898578b2a71ea666c7e75aa9b9c9e246d53
Jannik-dev <dev.janniknorden@gmail.com>
2022-08-18 13:35:27 +0200

    Specify some default configuration

--- TL1/config.sh
+++ TL1/config.sh
@@ -1,23 +1,30 @@
 #!/usr/bin/env bash
 
-$COVER="titlepage.pdf"                      # the name of the titlepage (has to be a pdf)
-$ABBREVIATIONS_FILE="abbreviations"   # the name of the abbreviations file (has to be json)
+$COVER="titlepage.pdf"
+#   ^  specifies the name of the titlepage (has to be a pdf)
 
-$MD_FILES="0_example.md"
+$ABBREVIATIONS_FILE="abbreviations"
+#   ^  specifies the name of the abbreviations file (has to be json)
+
+$MD_FILES=""
 #   ^  specifies the order of the markdown files (seperate them by spaces)
 
-$STYLES="style_de_arial.yaml style_de_tnr.yaml style_en_arial.yaml style_en_tnr.yaml"    
-# you should only use one language in this job - choose one (if you want both than create a new folder)
+$STYLES="style_de_arial.yaml style_de_tnr.yaml style_en_arial.yaml style_en_tnr.yaml"
+#   ^  specifies the styles to use
 
 
 
 
 
+NN_COVER="${COVER:-titlepage.pdf}"
+NN_ABBREVIATIONS_FILE="${ABBREVIATIONS_FILE:-abbreviations}"
+NN_MD_FILES="${MD_FILES:-*.md}"
+NN_STYLES="${STYLES:-style_de_arial.yaml style_de_tnr.yaml style_en_arial.yaml style_en_tnr.yaml}"
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 echo "{folder}={$SCRIPT_DIR}" >> $GITHUB_ENV
 
-echo "{titlepage}={$COVER}" >> $GITHUB_ENV
-echo "{abbreviations_file}={$ABBREVIATIONS_FILE}" >> $GITHUB_ENV
-echo "{md_files}={$MD_FILES}" >> $GITHUB_ENV
-echo "{styles}={$STYLES}" >> $GITHUB_ENV
+echo "{titlepage}={$NN_COVER}" >> $GITHUB_ENV
+echo "{abbreviations_file}={$NN_ABBREVIATIONS_FILE}" >> $GITHUB_ENV
+echo "{md_files}={$NN_MD_FILES}" >> $GITHUB_ENV
+echo "{styles}={$NN_STYLES}" >> $GITHUB_ENV
