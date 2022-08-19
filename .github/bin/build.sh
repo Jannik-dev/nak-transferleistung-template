e1667842261700756074dd67de8b7d4b1fe50100
Jannik-dev <dev.janniknorden@gmail.com>
2022-08-19 15:54:07 +0200

    Fix path issue

--- .github/bin/build.sh
+++ .github/bin/build.sh
@@ -3,20 +3,20 @@
 set -e
 
 function generateBibPage() {
-    echo "# ${BIB_TITLE} {-}" > ~/tmp/bib.md
-    echo "\leftskip=2em" >> ~/tmp/bib.md
-    echo "\parindent=-2em" >> ~/tmp/bib.md
+    echo "# ${BIB_TITLE} {-}" > ${HOME}/tmp/bib.md
+    echo "\leftskip=2em" >> ${HOME}/tmp/bib.md
+    echo "\parindent=-2em" >> ${HOME}/tmp/bib.md
 }
 
 function generateAbbrevPage() {
-    echo "# ${ACR_TITLE} {-}" > ~/tmp/acr.md
+    echo "# ${ACR_TITLE} {-}" > ${HOME}/tmp/acr.md
 }
 
 CONFIGURATIONS=$(find ~+ -type f -name 'properties.ini' -exec dirname -z "{}" \; | sed -z 's/$/\n/')
 
 for CONF in $CONFIGURATIONS; do
     CONF_FOLDER=$(basename ${CONF})
-    mkdir -p ~/tmp
+    mkdir -p ${HOME}/tmp
 
     CONF_COVER=$(sed -n 's/COVER=//p' $CONF/properties.ini)
     CONF_ABBREVIATIONS_FILE=$(sed -n 's/ABBREVIATIONS_FILE=//p' $CONF/properties.ini)
@@ -38,8 +38,10 @@ for CONF in $CONFIGURATIONS; do
         generateBibPage
         generateAbbrevPage
 
+
+
         echo "Build PDF for ${CONF_FOLDER} with ${STYLE}"
-        pandoc --from markdown --to=pdf --pdf-engine=xelatex --embed-resources --standalone --table-of-contents --bibliography=$CONF/${CONF_BIB_FILE} --citeproc --number-sections --filter pandoc-acro $CONF/${CONF_MD_FILES} --include-before-body=~/tmp/acr.md --metadata-file ${STYLE} $CONF/${CONF_MD_FILES} ~/tmp/bib.md -o ~/tmp/${STYLE}.pdf
+        pandoc --from markdown --to=pdf --pdf-engine=xelatex --embed-resources --standalone --table-of-contents --bibliography=$CONF/${CONF_BIB_FILE} --citeproc --number-sections --filter pandoc-acro $CONF/${CONF_MD_FILES} --include-before-body=${HOME}/tmp/acr.md --metadata-file ${STYLE} $CONF/${CONF_MD_FILES} ${HOME}/tmp/bib.md -o ~/tmp/${STYLE}.pdf
 
         echo "Combine PDF for ${CONF_FOLDER} with ${STYLE}"
         pdftk $CONF/${CONF_COVER} ~/tmp/${STYLE}.pdf cat output ~/build/${CONF_FOLDER}/${CONF_FOLDER}_${STYLE}.pdf
