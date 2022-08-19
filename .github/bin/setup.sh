de8c4899e72d55247c03b47664af77eb37481985
Jannik-dev <dev.janniknorden@gmail.com>
2022-08-19 10:21:34 +0200

    Fix mkdir - permission denied

--- .github/bin/setup.sh
+++ .github/bin/setup.sh
@@ -6,14 +6,14 @@ cd $1
 
 CONFIGURATIONS=$(find ~+ -type f -name 'properties.ini' -exec dirname -z "{}" \; | sed -z 's/$/\n/')
 
-mkdir -p build
+sudo mkdir -p build
 cd build
 
 for CONF in $CONFIGURATIONS; do
     CONF_FOLDER=$(basename ${CONF})
     echo "Creating dir build/${CONF_FOLDER}"
 
-    mkdir -p ${CONF_FOLDER}
+    sudo mkdir -p ${CONF_FOLDER}
 done
 
 cd ..
