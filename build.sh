3d4e3c99c8ec7adebf20f861a683f58b155fb871
Jannik-dev <dev.janniknorden@gmail.com>
2022-08-22 14:33:59 +0200

    Add local build

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -e
+
+docker start nakcontainer || docker run --name nakcontainer --detach -ti jnorden/nak-transferleistung-template:1.0.0
+docker cp . nakcontainer:/nak/
+rm -rf build/*
+docker exec nakcontainer /bin/bash -c "cd /nak && bash bin/setup_pdf.sh && bash bin/build_pdf.sh"
+docker cp nakcontainer:/tmp/build .
+docker stop nakcontainer
